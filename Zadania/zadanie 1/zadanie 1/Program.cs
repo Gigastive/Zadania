9e6366cdea12d52918e48d079e13cfeb27cc69ab
Gigastive <141347492+Gigastive@users.noreply.github.com>
2023-10-27 21:46:38 +0200

    Dodanie zadan

---
+++ Zadania/zadanie 1/zadanie 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace lol
+{
+    class program
+    {
+        static void Main()
+        {
+            int nor = 0;
+            char nar = 'a';
+            string text = File.ReadAllText("C:\\Users\\Praktyka\\Desktop\\test\\test_tomasz_lyzwa.txt");
+            char[] listaliter = text.ToCharArray();
+            for (int i = 0; i < listaliter.Length; i++)
+            {
+                char c = listaliter[i];
+                if (c == nar)
+                {
+                    nor++;
+                }
+            }
+            Console.WriteLine(nor);
+        }
+
+    }
+}
---
+++ Zadania/zadanie 4/zadanie 4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace lol
+{
+    class program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Podaj pln");
+            string pln = Console.ReadLine();
+            double kurs = 0.23;
+            double usd = Convert.ToInt32(pln) * kurs;
+            Console.WriteLine(usd);
+        }
+
+    }
+}
